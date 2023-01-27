8fb861bd39288d472c4df2d9d0d0679617da7d96
Jim Jorgensen <jim@uptrust.co>
2023-01-26 21:42:26 -0500

    visually got everything in the 3rd sction of the header aligned

--- src/logic/header.js
+++ src/logic/header.js
@@ -15,23 +15,23 @@ function Header() {
         <div className="airbnbLogo">
           <img alt="airbnb logo" height="50" width="100" src={logo}></img>
         </div>
-              <div className="headerMiddle">
-                  <div className="hMiddle">
-          <p className="marginRight"> Anywhere</p>
-          <p className="marginRight vLineBreak    ">Any week            </p>
-                  <p className="marginRight">Add guests</p>
-          <p className="svgIcon">
-            <img alt="search logo" height="30 " width="30" src={search}></img>
-                  </p>
-                 </div>
+        <div className="headerMiddle">
+          <div className="hMiddle">
+            <p className="marginRight"> Anywhere</p>
+            <p className="marginRight vLineBreak    ">Any week </p>
+            <p className="marginRight">Add guests</p>
+            <p className="svgIcon">
+              <img alt="search logo" height="30 " width="30" src={search}></img>
+            </p>
+          </div>
         </div>
         <div className="headerEnd">
-          <p className="marginRight2">Airbnb your home</p>
-          <p className="marginRight2">
+          <p className="marginRight2 headerEnd1">Airbnb your home</p>
+          <p className="marginRight2 headerEnd2">
             <img alt="world logo" height="25" width="25" src={world}></img>
           </p>
-          <p>
-            <img alt="login in logo" height="30" width="70" src={login}></img>
+          <p className="headerEnd3">
+            <img alt="login in logo" height="30" width="70" src={login} className="loginLogo"></img>
           </p>
           <div></div>
         </div>
